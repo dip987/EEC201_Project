4ac168cf6e1a5dff6b55a291f64461da38c9bfe8
Rishad Raiyan <45767121+dip987@users.noreply.github.com>
2021-03-15 20:43:39 -0700

    Add files via upload

--- classify.m
+++ classify.m
@@ -1,15 +1,18 @@
-function [speaker_number] = classify(lbg_codebook)
+function [speaker_number, all_distances] = classify(lbg_codebook)
 %Classify -> takes in the generated LBG Codebook and returns which speaker
 %it matches. Make sure the training "codebook.mat" exists before running
 %this function. Returns -1 if none 
 load './codebook.mat' codebook
 min_dist = inf; % Set this to the threshold value
 speaker_number = -1;
+all_distances = [];
 for i=1:length(codebook)
     dist = distance(codebook{i}, lbg_codebook);
+    all_distances = [all_distances dist]; %#ok<AGROW>
     if dist < min_dist
         min_dist = dist;
         speaker_number = i;
     end
 end
 disp(min_dist)
+
