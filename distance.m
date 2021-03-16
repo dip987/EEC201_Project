806e3b792f85f7f1ef4b1ed10e01ef897b7fa00a
Rishad Raiyan <45767121+dip987@users.noreply.github.com>
2021-03-15 20:51:49 -0700

    Square root

--- distance.m
+++ distance.m
@@ -4,7 +4,7 @@ total_dist = 0;
 for i=1:size(inputArg1(1))
     min_dist = inf;
     for j=1:size(inputArg2(1))
-        dist = sum((inputArg1(i, :) - inputArg2(j, :)).^2);
+        dist = sqrt(sum((inputArg1(i, :) - inputArg2(j, :)).^2));
         if dist<min_dist
             min_dist = dist;
         end
